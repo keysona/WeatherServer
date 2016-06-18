7927dcf8a84fe46ce7ee7e88f23ce5c4a05efbb8
keysona <keypython@gmail.com>
2016-06-18 18:08:07 +0800

    Add a log file.

--- spider.sh
+++ spider.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd WeatherSpider/WeatherSpider
-scrapy crawl provinces
-scrapy crawl cities
-scrapy crawl countries
+scrapy crawl --logfile=location.log -L ERROR provinces
+scrapy crawl --logfile=location.log -L ERROR cities
+scrapy crawl --logfile=location.log -L ERROR countries
+py.test
 cd ../..
