bbc65d55d247d290a427ac5ba2c43b9d0033654d
keysona <keypython@gmail.com>
2016-06-20 16:50:57 +0800

    Fix user's real ip address.

--- WeatherServer/weather/views.py
+++ WeatherServer/weather/views.py
@@ -6,6 +6,9 @@ weather = Blueprint('weather', __name__, url_prefix='/weather')
 
 @weather.route('/', methods=['GET'])
 def index():
-    ip = request.remote_addr
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remotw_addr
     location = IP.find(ip)
     return jsonify(location=location, ip=ip)
